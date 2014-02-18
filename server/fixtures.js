39859ef06039f7775392a7857961de9d125e1f14
seanrose <seanrose@alumni.stanford.edu>
2014-02-17 23:33:19 -0800

    added comments collections, pub sub and redid fixture

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,19 +1,54 @@
+// fixture data
 if (Posts.find().count() === 0) {
-    Posts.insert({
-        title: 'Introducing Telescope',
-        author: 'Sacha Greif',
-        url: 'http://sachagreif.com/introducing-telescope/'
+    var now = new Date().getTime();
+
+    // create two users
+    var derrickId = Meteor.users.insert({
+        profile: { name: 'Derrick Rose' }
+    });
+    var derrick = Meteor.users.findOne(derrickId);
+    var kevinId = Meteor.users.insert({
+        profile: { name: 'Kevin Durant' }
+    });
+    var kevin = Meteor.users.findOne(kevinId);
+
+    var kdThirtyFiveId = Posts.insert({
+        title: 'The Official Home of Kevin Durant',
+        userId: kevin._id,
+        author: kevin.profile.name,
+        url: 'http://kevindurant35.com/',
+        submitted: now - 7 * 3600 * 1000
+    });
+
+    Comments.insert({
+        postId: kdThirtyFiveId,
+        userId: derrick._id,
+        author: derrick.profile.name,
+        submitted: now - 5 * 3600 * 1000,
+        body: 'I may be injured but I\'m still better than you'
+    });
+
+    Comments.insert({
+        postId: kdThirtyFiveId,
+        userId: kevin._id,
+        author: kevin.profile.name,
+        submitted: now - 3 * 3600 * 1000,
+        body: 'LMAO #MVP'
     });
 
     Posts.insert({
-        title: 'Meteor',
-        author: 'Tom Coleman',
-        url: 'http://meteor.com'
+        title: 'Derrick Rose (HD)',
+        userId: derrick._id,
+        author: derrick.profile.name,
+        url: 'https://www.youtube.com/watch?v=GOtxJrzp6ls',
+        submitted: now - 10 * 3600 * 1000
     });
 
     Posts.insert({
-        title: 'The Meteor Book',
-        author: 'Tom Coleman',
-        url: 'http://themeteorbook.com'
+        title: 'Derrick Rose Stats',
+        userId: derrick._id,
+        author: derrick.profile.name,
+        url: 'http://espn.go.com/nba/player/_/id/3456/derrick-rose',
+        submitted: now - 12 * 3600 * 1000
     });
 }
