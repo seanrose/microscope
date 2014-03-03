3aee21d50cfc04575d33c00a47427f15ed69fa24
seanrose <seanrose@alumni.stanford.edu>
2014-03-02 21:53:47 -0800

    fade animation when items update

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -19,6 +19,34 @@ Template.postItem.helpers({
     }
 });
 
+Template.postItem.rendered = function(){
+    // animate post from previous position to new position
+    var instance = this;
+    var rank = instance.data._rank;
+    var $this = $(this.firstNode);
+    var postHeight = 80;
+    var newPosition = rank * postHeight;
+
+    // if element has a currentPosition (i.e. it's not the first ever rendered)
+    if (typeof(instance.currentPosition) !== 'undefined') {
+        var previousPosition = instance.currentPosition;
+        // calculate difference between old position and new position
+        // and send element there
+        var delta = previousPosition - newPosition;
+        $this.css('top', delta + 'px');
+    } else {
+        // it's the first ever render, so hide element
+        $this.addClass('invisible');
+    }
+
+    // let it draw in the old position, then
+    Meteor.defer(function() {
+        instance.currentPosition = newPosition;
+        // bring element ack to its new original position
+        $this.css('top', '0px').removeClass('invisible');
+    });
+};
+
 Template.postItem.events({
     'click .upvotable': function(e) {
         e.preventDefault();
